e23bc583c90a69eb02c61ecc4a3a10e3f9df7756
JulianoKlak <julianoklak@gmail.com>
2025-08-26 22:40:12 -0300

    interagindo com a aplicação

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+  cy.visit('./src/index.html')
+  cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.'
+    cy.visit('./src/index.html')
+    cy.get('#firstName').type('Juliano')
+    cy.get('#lastName').type('Santos')
+    cy.get('#email').type('teste@teste.com.br')
+    
+  })
+
 })
